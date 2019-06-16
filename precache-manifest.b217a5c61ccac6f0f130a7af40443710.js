92792c8dd07cf507934b44bfb8391afa9b4f2ede
NAGASWAMY <uma.nagaswamy@agilent.com>
2019-06-15 19:01:48 -0500

    Updates

---
+++ precache-manifest.b217a5c61ccac6f0f130a7af40443710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf53397a74cb4f22b8210d629056050",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "7521f18111be41cef1dc",
+    "url": "/react-redux-todo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "41a02f91a1fab48a71fb",
+    "url": "/react-redux-todo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7521f18111be41cef1dc",
+    "url": "/react-redux-todo/static/js/2.938b4033.chunk.js"
+  },
+  {
+    "revision": "41a02f91a1fab48a71fb",
+    "url": "/react-redux-todo/static/js/main.bdfd8aca.chunk.js"
+  },
+  {
+    "revision": "d2f5988937e3f62cf54b",
+    "url": "/react-redux-todo/static/js/runtime~main.620750b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
